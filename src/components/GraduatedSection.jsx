87aff68a02b3103fbacc98b20b1b8420fb79a975
Arief <93286382+AripHakim@users.noreply.github.com>
2025-05-17 00:45:44 +0800

    Update GraduatedSection.jsx

--- src/components/GraduatedSection.jsx
+++ src/components/GraduatedSection.jsx
@@ -11,7 +11,6 @@ const GraduatedSection = ({ id }) => {
   useEffect(() => {
     const fetchGraduates = async () => {
       try {
-        // const response = await fetch('https://maleo-be.onrender.com/lulus.json');
         const response = await fetch('https://maleo-be.onrender.com/api/lulus');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -32,7 +31,7 @@ const GraduatedSection = ({ id }) => {
                   .trim()
               : null;
             
-            let imageUrl = '/logo-maleo.jpg';
+            let imageUrl = '/logo.png';
             if (item["写真"] && item["写真"].includes('drive.google.com')) {
               const match = item["写真"].match(/\/file\/d\/([^\/]+)/);
               if (match && match[1]) {
@@ -163,10 +162,10 @@ const GraduatedSection = ({ id }) => {
                       className="absolute inset-0 w-full h-full object-cover object-[10%_18%]"
                       onError={(e) => {
                         e.target.onerror = null; 
-                        e.target.src = "/logo-maleo.jpg";
+                        e.target.src = "/logo.png";
                       }}
                     />
-                    {graduate.image === '/logo-maleo.jpg' && (
+                    {graduate.image === '/logo.png' && (
                       <div className="absolute inset-0 flex items-center justify-center text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
